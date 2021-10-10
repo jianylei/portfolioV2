3f9e486e3fcb07e04154c7fa358693fa1f37573f
jianylei <jianyonglei@gmail.com>
2021-10-09 23:43:34 -0400

    completed home page

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,28 @@
+import "./main.css"
+
+export default function Main() {
+    return (
+        <div className="main-container">
+            <div className="dev-text-container">
+                <span className="dev-text">developer.</span>
+            </div>
+            <div className="main-body">
+                <h4>JIAN LEI</h4>
+                <h2>Web Developer</h2>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi laudantium harum commodi. Nulla unde quaerat consequuntur accusantium molestiae laudantium similique pariatur ullam quas tempora. Magnam, maxime exercitationem! Veniam, earum. Repellendus sit architecto consequuntur ad provident eveniet nesciunt quod aspernatur autem. Nulla eum, exercitationem tenetur iste et rem enim perferendis illum.
+                </p>
+                <div class="social-links">
+                    <a href="https://github.com/jianylei" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="mailto:jianyonglei@gmail.com"><i class="fas fa-envelope-square"></i></a>
+                    <a href="https://www.linkedin.com/in/jianylei/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                </div>
+                <span>
+                    <h3>View Projects</h3> or
+                    <h3>Read About Me</h3> or
+                    <h3>Say Hi!</h3>
+                </span>
+            </div>
+        </div>
+    )
+}
