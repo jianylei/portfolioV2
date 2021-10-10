0740d93a79ce641cc6c3c117f7c6b22ae460c045
jianylei <jianyonglei@gmail.com>
2021-10-10 09:46:56 -0400

    home fixed alignment + color

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -12,16 +12,17 @@ export default function Main() {
                 <p>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi laudantium harum commodi. Nulla unde quaerat consequuntur accusantium molestiae laudantium similique pariatur ullam quas tempora. Magnam, maxime exercitationem! Veniam, earum. Repellendus sit architecto consequuntur ad provident eveniet nesciunt quod aspernatur autem. Nulla eum, exercitationem tenetur iste et rem enim perferendis illum.
                 </p>
-                <div className="social-links">
-                    <a href="https://github.com/jianylei" target="_blank"><i className="fab fa-github"></i></a>
-                    <a href="mailto:jianyonglei@gmail.com"><i className="fas fa-envelope-square"></i></a>
-                    <a href="https://www.linkedin.com/in/jianylei/" target="_blank"><i className="fab fa-linkedin"></i></a>
-                </div>
+                
                 <span>
                     <h3>View Projects</h3> or
                     <h3>Read About Me</h3> or
                     <h3>Say Hi!</h3>
                 </span>
+                <div className="social-links">
+                <a href="mailto:jianyonglei@gmail.com"><i className="fas fa-envelope-square"></i></a>
+                    <a href="https://github.com/jianylei" target="_blank"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/jianylei/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                </div>
             </div>
         </div>
     )
