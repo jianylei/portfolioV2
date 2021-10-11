fde24dcf054be9377a083e0bd33dda87d1f5ed70
jianylei <jianyonglei@gmail.com>
2021-10-10 23:52:55 -0400

    finished contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,15 @@ export default function Contact() {
         <div className="contact-container" id="contact-container">
             <h4>need a developer?</h4>
             <h2>Contact Me</h2>
+                <div className="contact-form">
+                  <form action="https://formspree.io/f/mzbyzqzz" method="POST">
+                        <input type="text" placeholder="Name" name="name" className="input-control" required/>
+                        <input type="text" placeholder="Email" name="email" className="input-control" required/>
+                        <input type="text" placeholder="Subject" name="subject" className="input-control" required/>
+                        <textarea placeholder="Message" name="message" className="input-control" required></textarea>
+                        <button className="btn" type="submit">send message</button>
+                  </form>
+              </div>
         </div>
     )
 }
