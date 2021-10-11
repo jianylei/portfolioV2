f04eb11425616e16442508120bd3e0d23f43e298
jianylei <jianyonglei@gmail.com>
2021-10-10 22:09:47 -0400

    fixed projects responsiveness

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -5,6 +5,11 @@ export default function About() {
         <div className="about-container" id="about-container">
             <h4>my journey</h4>
             <h2>About</h2>
+            <p className="about-text">
+                Hi, my name is Jian! I'm currently attending Seneca as part of the Computer Programming & Analysis (CPA) program. Throughout my time at Seneca, I have gained experience and extensive knowledge on skills such as object-orientated programming with C++, and full stack web development using Node.js, Express.js, React, and MongoDB. I have also gained a strong understanding of relational databases - from creating and implementing a schema - to implementing it within an application, and then to interact with the data using SQL. 
+                <br/> <br/>
+                I have a curious mind and enojoy work that can challenge me to learn something new, whether it be a new language or technology. I am always on the lookout for exciting projects to work on!
+            </p>
         </div>
     )
 }
