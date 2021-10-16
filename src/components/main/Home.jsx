2578b2a10fcd38e4653dea482f2cc1484e76afa7
jianylei <jianyonglei@gmail.com>
2021-10-16 02:55:59 -0400

    added btn to download resume

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,5 +1,5 @@
 import "./home.css"
-
+import cv from "../../cv/cv.pdf"
 export default function Home() {
     return (
             <div className="main-body">
@@ -18,9 +18,10 @@ export default function Home() {
                     <a href="#contact-container"><h3>Say Hi!</h3></a>
                 </span>
                 <div className="social-links">
-                <a href="mailto:jianyonglei@gmail.com"><i className="fas fa-envelope-square"></i></a>
+                    <a href="mailto:jianyonglei@gmail.com"><i className="fas fa-envelope-square"></i></a>
                     <a href="https://github.com/jianylei" rel="noopener noreferrer" target="_blank"><i className="fab fa-github"></i></a>
                     <a href="https://www.linkedin.com/in/jianylei/" rel="noopener noreferrer" target="_blank"><i className="fab fa-linkedin"></i></a>
+                    <a href={cv} download="jianlei_resume" target="_blank" rel="noopener noreferrer" class="btn"><i class="fas fa-download"></i></a>
                 </div>
             </div>
     )
