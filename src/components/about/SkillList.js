d1696629eb020718233c57f17286fcd406e13617
jianylei <jianyonglei@gmail.com>
2021-10-16 14:27:46 -0400

    updated skills

--- src/components/about/SkillList.js
+++ src/components/about/SkillList.js
@@ -11,7 +11,7 @@ const skillsList = [
             },{
                 name: "Java*"
             },{
-                name: "TypeScript*"
+                name: "Python*"
             }
         ]
     },{
